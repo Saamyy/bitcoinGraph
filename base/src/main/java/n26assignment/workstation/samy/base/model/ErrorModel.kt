5f6b933bdfdedf97ee989ac0de21d3c7e221d05e
Lsamy94 <mahmoud-samy94@hotmail.com>
2019-01-04 15:32:03 +0200

    implement the basePresenter

---
+++ base/src/main/java/n26assignment/workstation/samy/base/model/ErrorModel.kt
@@ -0,0 +1,3 @@
+package n26assignment.workstation.samy.base.model
+
+data class ErrorModel(val status:String, val error:String?)
---
+++ base/src/main/java/n26assignment/workstation/samy/base/presenter/BasePresenter.kt
@@ -0,0 +1,29 @@
+package n26assignment.workstation.samy.base.presenter
+
+import android.arch.lifecycle.MutableLiveData
+import android.content.Context
+import n26assignment.workstation.samy.base.model.ErrorModel
+
+abstract class BasePresenter <V>{
+    private var  view: V? =null
+    private lateinit var context:Context
+    private var errorModel:MutableLiveData<ErrorModel> = MutableLiveData()
+
+    fun attachView(view: V,context: Context) {
+        this.view = view
+        this.context=context
+        onViewAttached()
+    }
+
+    fun detachView() {
+        this.view = null
+        onViewDetached()
+    }
+    fun view() = view
+    fun context()=context
+    fun errorModel()=errorModel
+
+    abstract fun onViewAttached()
+
+    open fun onViewDetached(){}
+}
