06f2a0929c4e56180eb811a3f096fdf0d0b07471
Lsamy94 <mahmoud-samy94@hotmail.com>
2019-01-04 14:23:33 +0200

    initial commit with the required dependencies

---
+++ app/src/main/java/n26assignment/workstation/samy/n26assignment/BitcoinChartActivity.kt
@@ -0,0 +1,12 @@
+package n26assignment.workstation.samy.n26assignment
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BitcoinChartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bitcoin_chart)
+    }
+}
