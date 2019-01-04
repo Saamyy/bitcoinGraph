79a3b8115be3ee37f2228b9a5eb7c23edc1015c3
Lsamy94 <mahmoud-samy94@hotmail.com>
2019-01-04 19:47:14 +0200

    implement the market price useCaseLayer

---
+++ app/src/main/java/n26assignment/workstation/samy/n26assignment/Period.kt
@@ -0,0 +1,5 @@
+package n26assignment.workstation.samy.n26assignment
+
+enum class Period(val value: String) {
+    ONE_MONTH("30days"), TWO_MONTH("60days"), YEAR("1year")
+}
---
+++ app/src/main/java/n26assignment/workstation/samy/n26assignment/usecase/MarketPriceUseCase.kt
@@ -0,0 +1,36 @@
+package n26assignment.workstation.samy.n26assignment.usecase
+
+import android.arch.lifecycle.MutableLiveData
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import n26assignment.workstation.samy.base.Model.ErrorModel
+import n26assignment.workstation.samy.base.Model.MarketPrice
+import n26assignment.workstation.samy.n26assignment.DataSource.MarketPriceRepository
+import n26assignment.workstation.samy.n26assignment.Period
+
+class MarketPriceUseCase(
+    var marketPriceList: MutableLiveData<List<MarketPrice>>,
+    var errorModel: MutableLiveData<ErrorModel>
+) {
+    private var marketPriceRepository: MarketPriceRepository = MarketPriceRepository()
+    private val bag: CompositeDisposable = CompositeDisposable()
+
+    fun getMarket(historyPeriod: Period) {
+        val disposable = marketPriceRepository.getMarketPrice(historyPeriod.value)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe({
+                marketPriceList.value = it.values
+            }, {
+                errorModel.value = it as ErrorModel
+            })
+        bag.add(disposable)
+    }
+
+    fun DisposeObservable() {
+        bag.clear()
+    }
+
+
+}
