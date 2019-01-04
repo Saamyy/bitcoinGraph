b2fd1729c2ff8512d94ea6873d5e25f726141744
Lsamy94 <mahmoud-samy94@hotmail.com>
2019-01-04 15:32:53 +0200

    implement the BaseActivity

---
+++ base/src/main/java/n26assignment/workstation/samy/base/view/BaseActivity.kt
@@ -0,0 +1,33 @@
+package n26assignment.workstation.samy.base.view
+
+import android.support.annotation.CallSuper
+import android.support.v7.app.AppCompatActivity
+import n26assignment.workstation.samy.base.presenter.BasePresenter
+
+abstract class BaseActivity<P : BasePresenter<V>, V> : AppCompatActivity() {
+
+    abstract fun view(): V
+
+    abstract fun presenter(): P
+
+
+    @CallSuper
+    override fun onResume() {
+        super.onResume()
+        attachPresenter()
+    }
+
+    @CallSuper
+    override fun onPause() {
+        super.onPause()
+        detachPresenter()
+    }
+
+    @CallSuper
+    open fun attachPresenter() = presenter().attachView(view(), this)
+
+    @CallSuper
+    open fun detachPresenter() = presenter().detachView()
+
+
+}
